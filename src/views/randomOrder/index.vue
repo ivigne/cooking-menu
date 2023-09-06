deaab8d977be34304f7d59c2b1407ab8b89e04b7
vigne <1186963387@qq.com>
2023-09-06 20:23:34 +0800

    feat: 随机点菜

--- src/views/randomOrder/index.vue
+++ src/views/randomOrder/index.vue
@@ -2,7 +2,7 @@
  * @Author: vigne 1186963387@qq.com
  * @Date: 2023-08-07 17:22:10
  * @LastEditors: vigne 1186963387@qq.com
- * @LastEditTime: 2023-08-07 18:23:21
+ * @LastEditTime: 2023-09-06 20:23:20
  * @FilePath: /cooking-menu/src/views/randomOrder/index.vue
  * @Description: 
  * 
@@ -10,26 +10,49 @@
 -->
 <template>
   <PageWrapper contentFullHeight fixedHeight>
-    <div>{{ orderInput }}</div>
-    <div ref="startOrdering" onclick="startOrdering()">{{ startOrderingText }}</div>
+    <Card hoverable style="width: 300px">
+      <template #cover>
+        <Image :alt="result?.title" :src="result?.imgUrl" />
+      </template>
+      <template #title>{{ result?.title }}</template>
+      <template #actions>
+        <Space direction="vertical">
+          <Select
+            v-bind="selectValue"
+            @change="handleChange"
+            size="small"
+            :options="menuOptions"
+            show-search
+            placeholder="支持搜索"
+            :filter-option="filterOption"
+          />
+          <Button @click="startOrdering()"> {{ startOrderingText }} </Button>
+        </Space>
+      </template>
+      <!-- <CardMeta>
+                <template #title>
+                  <TypographyText :content="item.name" :ellipsis="{ tooltip: item.address }" />
+                </template>
+                <template #description>{{ item.time }}</template>
+              </CardMeta> -->
+    </Card>
   </PageWrapper>
 </template>
-<script lang="ts">
-  export default {
-    name: 'RandomOrder',
-  };
-</script>
 <script lang="ts" setup>
   import { ref } from 'vue';
+  import { Select, Card, Image, Button, Space } from 'ant-design-vue';
   // import { useI18n } from '/@/hooks/web/useI18n';
   // import { useMessage } from '/@/hooks/web/useMessage';
   // const { createMessage } = useMessage();
 
   // const { t } = useI18n();
-  const orderInput = ref<any>('艾瑞巴蒂，准备好了吗？');
+  const selectValue = ref<any>(null);
+  const menuOptions = ref<any>(null);
   const startOrderingText = ref<any>('开始点菜');
-  // const startOrdering = ref<any>();
   const mytime = ref<any>(null);
+  const result = ref<any>({
+    title: '艾瑞巴蒂，准备好了吗？',
+  });
 
   const namelist = ['吴亦凡', '王俊凯', '王一博', '刘昊然', '黄俊捷', '何洛洛', '刘特', '许凯'];
   const startOrdering = () => {
@@ -43,9 +66,15 @@
     }
   };
   const show = () => {
-    var num = Math.floor(Math.random() * 100) % namelist.length;
-    console.log(num);
-    orderInput.value = namelist[num];
+    var i = Math.floor(Math.random() * 100) % namelist.length;
+    console.log(i);
+    result.value = namelist[i];
     mytime.value = setTimeout('show()', 1);
+  };
+  const handleChange = (val) => {
+    console.log(val);
+  };
+  const filterOption = (input: string, option: any) => {
+    return option.value.toLowerCase().indexOf(input.toLowerCase()) >= 0;
   };
 </script>
