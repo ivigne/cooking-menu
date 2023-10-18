f69701c03caa3638053d25fc316fb38c5783755a
vigne <1186963387@qq.com>
2023-10-18 09:49:19 +0800

    feat: 贵州，海南

--- src/views/asia/eastAsia/china/mockData/hainanProvince/danzhou.ts
+++ src/views/asia/eastAsia/china/mockData/hainanProvince/danzhou.ts
@@ -1,12 +1,15 @@
 /**
  * 海南-儋州市-特产美食
- * 特产：
- * 美食：灯影牛肉、尖椒肥肠、粉蒸鲶鱼、江阳酸辣鸡、大风羊肉、面筋团、羊肉格格、石梯蒸鱼、徐鸭子、麻辣鸡块、干烧岩鲤、牛肉松、什锦烩面、
- * 酸菜鱼、石锅鱼、顺江薄饼、杜仲腰花、宣汉牛肉、小煎仔鸡、古今香丸子、老腊肉、水八块、鱼香茄花、油酥竹鼬、石桥米酒鱼、麻柳蒋氏南瓜鸭、
- * 红海游龙酸辣粉、三圣宫七星椒卤味、凉面锅盔、鸡豆花、五香卤豆干、松菌炖鸡、鸡油大汤圆、米豆腐、沸腾鹅肠、素烧阳鱼、三鲜塔丝饼、功夫牛肉、
- * 清明菜粑粑、口水鸡、开江臊子面、肉丁面、东柳鱼头、石桥米酒鱼、巴山吊锅、糯米油子、东柳醪糟、三汇果醋、呷酒、观音豆干、大竹香椿、儋州橄榄油、
- * 米城大米、凉粉、凤凰柚、万源马铃薯、旧院黑鸡、开江麻鸭、开江白鹅、达县脐橙、达县苎麻、达县乌梅、儋州脆李、达川安仁柚、龙会红桔、安仁甜柚、
- * 宣汉桃花米、巴山雀舌、宣汉黄牛、老君香菇、旧院黑鸡蛋、蜀宣花牛、三汇特醋、 蜂桶蜂蜜、龙会红桔、板角山羊
+新英红鱼粽
+红鱼粽又称鱼筒，是以新鲜红鱼加工成的腌制品。3斤左右的红鱼，掏出内脏腌制，成品外观整齐，不留刀痕，肉质松软，芳香可口，历来誉为佐餐佳品。红鱼粽以新英镇产的出名。
+洛基粽子
+北方的粽为圆锥形，多用苇叶包得小巧玲珑，内装糯米，有花样的再在粽中加上赤豆、红枣、火腿等，多以甜味为主。海南的粽则与内地不同，多用芭蕉叶包成方锥形，重在半公斤左右，糯米中有咸蛋黄、叉烧、腊肉、红烧鸡翅等，多以咸味为主。
+长坡米烂
+长坡米烂又称长坡米粉。其以优质大米制成细条状食品，宛如云南“过桥米线”，佐以配料，风味可口。
+松涛鳙鱼
+松涛水库是儋州市著名的旅游区，鳙鱼是水库的名鱼，它在水库中自然长成。鳙鱼俗称大头鱼，其头大身小，肉质细嫩，而鳙鱼头最好吃。
+光村沙虫
+沙虫貌似蚯蚓，生活在浅海沙泥中。沙虫营养丰富，可鲜食，也可加工成沙虫干。沙虫在市境内沿海海滩均有出产，但不如光村出产的质优，个儿肥软，肉厚味佳。光村除盛产沙虫外，还出产泥虫。
  */
 import { lampshadedBeefScriptsList } from './templateFoodScriptContent/lampshadedBeef';
 export const danzhouFoodsList = [
@@ -21,8 +24,8 @@ export const danzhouFoodsList = [
     foodName_en: '', // 美食英文名称
     foodCategoryCode: 'beef', // 食材类别编码
     foodCategoryName: '牛肉', // 食材类别名称
-    cuisineCategoryCode: 'sichuanCuisine', // 菜系编码
-    cuisineCategoryName: '川菜', // 所属菜系
+    cuisineCategoryCode: 'qiongCuisine', // 菜系编码
+    cuisineCategoryName: '琼菜', // 所属菜系
     tasteCode: 'spicy', // 口味编码
     tasteName: '麻辣', // 口味名称
     cookingTypeCode: 'multiple', // 烹饪类型编码
@@ -75,8 +78,8 @@ export const danzhouFoodsList = [
     foodName_en: '', // 美食英文名称
     foodCategoryCode: 'beef', // 食材类别编码
     foodCategoryName: '牛肉', // 食材类别名称
-    cuisineCategoryCode: 'sichuanCuisine', // 菜系编码
-    cuisineCategoryName: '川菜', // 所属菜系
+    cuisineCategoryCode: 'qiongCuisine', // 菜系编码
+    cuisineCategoryName: '琼菜', // 所属菜系
     tasteCode: 'spicy', // 口味编码
     tasteName: '麻辣', // 口味名称
     cookingTypeCode: 'multiple', // 烹饪类型编码
--- src/views/asia/eastAsia/china/mockData/hainanProvince/sansha.ts
+++ src/views/asia/eastAsia/china/mockData/hainanProvince/sansha.ts
@@ -21,8 +21,8 @@ export const sanshaFoodsList = [
     foodName_en: '', // 美食英文名称
     foodCategoryCode: 'beef', // 食材类别编码
     foodCategoryName: '牛肉', // 食材类别名称
-    cuisineCategoryCode: 'sichuanCuisine', // 菜系编码
-    cuisineCategoryName: '川菜', // 所属菜系
+    cuisineCategoryCode: 'qiongCuisine', // 菜系编码
+    cuisineCategoryName: '琼菜', // 所属菜系
     tasteCode: 'spicy', // 口味编码
     tasteName: '麻辣', // 口味名称
     cookingTypeCode: 'multiple', // 烹饪类型编码
@@ -75,8 +75,8 @@ export const sanshaFoodsList = [
     foodName_en: '', // 美食英文名称
     foodCategoryCode: 'beef', // 食材类别编码
     foodCategoryName: '牛肉', // 食材类别名称
-    cuisineCategoryCode: 'sichuanCuisine', // 菜系编码
-    cuisineCategoryName: '川菜', // 所属菜系
+    cuisineCategoryCode: 'qiongCuisine', // 菜系编码
+    cuisineCategoryName: '琼菜', // 所属菜系
     tasteCode: 'spicy', // 口味编码
     tasteName: '麻辣', // 口味名称
     cookingTypeCode: 'multiple', // 烹饪类型编码
