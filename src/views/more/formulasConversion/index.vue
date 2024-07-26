482b6568ecdf9b56fb7f07ddf53ef14a91f86d73
vigne <1186963387@qq.com>
2024-07-26 19:19:04 +0800

    feat: 样式优化

--- src/views/more/formulasConversion/index.vue
+++ src/views/more/formulasConversion/index.vue
@@ -2,7 +2,7 @@
  * @Author: vigne 1186963387@qq.com
  * @Date: 2022-08-24 09:46:17
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2024-07-26 18:11:27
+ * @LastEditTime: 2024-07-26 19:17:53
  * @FilePath: /cooking-menu/src/views/more/formulasConversion/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -130,8 +130,8 @@
         }
 
         .value {
-          // padding: 0 10px;
-          width: 60px;
+          padding: 0 5px;
+          min-width: 30px;
           font-size: 20px;
           font-weight: 500;
           line-height: 28px;
